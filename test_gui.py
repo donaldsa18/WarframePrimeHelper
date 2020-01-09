8356b7803d51dffc07d8f38924e5238c9f897c12
Anthony Donaldson <donaldsa18@up.edu>
2020-01-08 23:48:06 -0800

    Trying to setup xvfb

--- test_gui.py
+++ test_gui.py
@@ -5,7 +5,8 @@ def test_window(qtbot):
     app = App()
     app.create_window()
     app.init_threads()
-    qtbot.addWidget(app.window)
+    #qtbot.addWidget(app.window)
+
     assert app.window.isVisible()
     assert app.window.windowTitle() == "Warframe Prime Helper"
 
