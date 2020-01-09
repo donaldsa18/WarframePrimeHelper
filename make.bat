e8b3f0b8b2b6bfcfe52ee080f4e72185d672b60f
Anthony Donaldson <donaldsa18@up.edu>
2020-01-08 22:14:21 -0800

    Removed fbs so python 3.8 can be used

--- make.bat
+++ make.bat
@@ -1,5 +1,10 @@
 copy *.py src\main\python\
 rmdir /S /Q "target\Warframe Prime Helper\"
+for %%X in (myExecutable.exe) do (set FOUND=%%~$PATH:X)
+if not defined FOUND (
+    pip install fbs
+)
+
 fbs freeze %1
 xcopy "resources" "target\Warframe Prime Helper\resources\" /s /i
 xcopy "tesseract4win64-4.0-beta" "target\Warframe Prime Helper\tesseract4win64-4.0-beta\" /s /i
